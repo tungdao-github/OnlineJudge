520c21a6d0f532c2c94afe1522d520c63939f795
dao van tung <tungdao123pzo@gmail.com>
2025-04-05 18:41:11 +0700

    update project

---
+++ Data/JudgeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineJudge.Controllers;
+using OnlineJudge.Models;
+using System.Collections.Generic;
+
+namespace OnlineJudge.Data
+{
+    public class JudgeDbContext : DbContext
+    {
+        public JudgeDbContext(DbContextOptions<JudgeDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Problem> Problems { get; set; }
+        public DbSet<Submission> Submissions { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineJudge.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
