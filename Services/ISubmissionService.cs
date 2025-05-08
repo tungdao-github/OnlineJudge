c5378f12e00e3b9d5b38c3c0a431af9fde05ccdf
dao van tung <tungdao123pzo@gmail.com>
2025-05-08 17:44:21 +0700

    Create ISubmissionService.cs

---
+++ Services/ISubmissionService.cs
@@ -0,0 +1,10 @@
+
+using OnlineJudgeAPI.Models;
+
+public interface ISubmissionService
+{
+   
+        //Task RunSubmissionAsync(SubmissionRequest request, string connectionId);
+    
+    //Task<List<SubmissionResult>> SubmitCodeAsync(string code, int problemId, string language, string connectionId, int submissionId);
+}
